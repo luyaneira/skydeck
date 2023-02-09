b745b5c20d401b1bc6790ec46ba2e3173204b95e
Nancy Meike <nancy.meike@deutschebahn.com>
2023-02-09 17:28:46 +0100

    updated script

--- src/skydeck_bln.js
+++ src/skydeck_bln.js
@@ -1,6 +1,3 @@
-import { bootstrapExtra } from "@workadventure/scripting-api-extra";
-
-// Calling bootstrapExtra will initiliaze all the "custom properties"  
-bootstrapExtra();
+import { } from "https://unpkg.com/@workadventure/scripting-api-extra@^1";
 
 console.log('Script started successfully');
