b8ac19e3a46fd04b3cde4cfa42aa5b7d3882b557
Nancy Meike <nancy.meike@deutschebahn.com>
2023-02-09 16:50:43 +0100

    changed script to js

---
+++ src/skydeck_bln.js
@@ -0,0 +1,6 @@
+import { bootstrapExtra } from "@workadventure/scripting-api-extra";
+
+// Calling bootstrapExtra will initiliaze all the "custom properties"  
+bootstrapExtra();
+
+console.log('Script started successfully');
